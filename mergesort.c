a6dd8c8be1ae696c78502bf1ac2c94738a7fe208
Diego Portillo <djportillo@taxit.com.py>
2023-06-13 09:01:40 -0400

    Merge Sort en c

--- mergesort.c
+++ mergesort.c
@@ -7,37 +7,41 @@
 int main()
 {
   int i;
-  int n=5;  
-  int arr[]={100, 1992, 0, 5, -1, 60, 70, 14, 15, 10};
+  int n=6;  
+  int arr[]={100, 1992, 0, 5, -1, 888};
 
-  merge_sort(arr,0,n-1);  // sort the array
+  printf("Unsorted array:\n");  // print unsorted array
+  for(i=0;i<n;i++)
+    printf("%d \n",arr[i]);
+
+  merge_sort(arr,0,n-1,n);  // sort the array
   
-  printf("Sorted array:");  // print sorted array
+  printf("Sorted array:\n");  // print sorted array
   for(i=0;i<n;i++)
     printf("%d \n",arr[i]);
   
   return 0;
 }
 
-int merge_sort(int arr[],int low,int high)
+int merge_sort(int arr[],int low,int high, int length)
 {
   int mid;
   if(low<high)
   {
     mid=(low+high)/2;
    // Divide and Conquer
-    merge_sort(arr,low,mid);
-    merge_sort(arr,mid+1,high);
+    merge_sort(arr,low,mid, length);
+    merge_sort(arr,mid+1,high, length);
    // Combine
-    merge(arr,low,mid,high);
+    merge(arr,low,mid,high, length);
   }
   
   return 0;
 }
 
-int merge(int arr[],int l,int m,int h)
+int merge(int arr[],int l,int m,int h, int length)
 {
-  int arr1[10],arr2[10];  // Two temporary arrays to hold the two arrays to be merged
+  int arr1[length],arr2[length];  // Two temporary arrays to hold the two arrays to be merged
   int n1,n2,i,j,k;
   n1=m-l+1;
   n2=h-m;
