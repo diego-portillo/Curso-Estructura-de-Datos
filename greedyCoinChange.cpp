90fc146953325fcded4a1e74606a9c09fac340bd
Diego Portillo <djportillo@taxit.com.py>
2023-06-18 11:17:58 -0400

    greedy, quicksort and custom_sort

---
+++ greedyCoinChange.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int greedyChange(int coinSet[], int n, int N){
+    if (N==0)
+        return 0;
+    if (N<0)
+        return INT_MAX;
+
+    int coins = INT_MAX;
+
+    for(int i = 0; i<n; i++)
+    {
+        int res = greedyChange(coinSet, n, N-coinSet[i]);
+        if(res != INT_MAX)
+            coins = min(coins, res+1);
+    }
+    return coins;
+}
+int main(int argc, char const *argv[]){
+    int coinSet[]={1,5,10,15,20};
+    int n = sizeof(coinSet)/sizeof(coinSet[0]);
+    int N = 15;
+    cout << "el minimo de monedas para dar cambio es: "
+            << greedyChange(coinSet,n,N);
+}
